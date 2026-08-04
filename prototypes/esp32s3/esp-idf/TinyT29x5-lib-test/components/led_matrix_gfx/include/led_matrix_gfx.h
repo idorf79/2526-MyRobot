31deba36f494a7663fd0c146b56ae8d33e6a75bd
Frido Steffers <frido.steffers@icloud.com>
2026-08-04 20:34:41 +0200

    added some colours

--- prototypes/esp32s3/esp-idf/TinyT29x5-lib-test/components/led_matrix_gfx/include/led_matrix_gfx.h
+++ prototypes/esp32s3/esp-idf/TinyT29x5-lib-test/components/led_matrix_gfx/include/led_matrix_gfx.h
@@ -21,6 +21,9 @@ typedef struct {
 #define LM_RED     LM_COLOR(255, 0, 0)
 #define LM_GREEN   LM_COLOR(0, 255, 0)
 #define LM_BLUE    LM_COLOR(0, 0, 255)
+#define LM_YELLOW  LM_COLOR(255, 255, 0)
+#define LM_ORANGE  LM_COLOR(255, 165, 0)
+#define LM_DARK_ORANGE  LM_COLOR(255, 140, 0)
 
 // How the single LED strip is physically wired into the width x height grid.
 // The strip either runs in complete ROWS (each traversal is `width` pixels
